f21e6da471e4bb7c874bf84f886af2912f5f2084
CrazyChenz <crazychenz@gmail.com>
2020-04-30 09:20:32 -0400

    Added helper scripts

--- serve.sh
+++ serve.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run -ti -v $(pwd):/home/user/work -p 0.0.0.0:4000:4000/tcp --rm crazychenz/jekyll_builder /bin/bash -c \
-    'export GEM_HOME=/home/user/gems ; PATH=/home/user/gems/bin:$PATH ; bundler exec jekyll serve --host 0.0.0.0 --verbose --watch --trace'
+    'export LC_ALL=C.UTF-8; export GEM_HOME=/home/user/gems ; PATH=/home/user/gems/bin:$PATH ; bundler exec jekyll serve --host 0.0.0.0 --verbose --watch --trace'
 
